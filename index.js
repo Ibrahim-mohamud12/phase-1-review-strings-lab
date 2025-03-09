7e37be407d788f71f934714e86956f6cf6f0dbc9
ibrahim <ibrahim.mohamud1@student.moringaschool.com>
2025-03-09 11:12:18 +0300

    complete string lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const shortGreeting = "Hello, world 'G'!" ;
+
+let welcomeMessage = `Welcome to our website, ${currentUser} 'Welcome to Flatbook,'!`;
+
+welcomeMessage // "Welcome to Flatbook, Grace Hopper!"
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+welcomeMessage // "Welcome to Flatbook, Grace Hopper!"
+
+
